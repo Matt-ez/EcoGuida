9888ad5773bc1ea5b97c5e8341845d48469f2f7b
Asfaltinho <83889522+Asfaltinho@users.noreply.github.com>
2025-05-24 10:41:49 +0200

    fixati pulsanti

--- src/main.py
+++ src/main.py
@@ -12,29 +12,64 @@ class Window(QWidget):
         self.setWindowTitle("EcoGuida 2025")
         self.setWindowIcon(QIcon("images/Logo_EcoGuida.png"))
         self.show()
+        self.deSelect()
+        self.selected = False
         self.pushSelect.clicked.connect(self.parcoChanger)
-        self.comboParchi.currentTextChanged.connect(lambda: self.comboParchi.setStyleSheet("QComboBox { background-color: #ffffff}"))
-        
+        self.comboParchi.currentTextChanged.connect(self.deSelect)
+    
+    def deSelect(self):
+        self.selected = False
+        self.label.setText("")
+        self.pushFlora.clicked.connect(self.alert)
+        self.pushFauna.clicked.connect(self.alert)
+        self.pushActivity.clicked.connect(self.alert)
+        self.pushRegole.clicked.connect(self.alert)
+        self.comboParchi.setStyleSheet("QComboBox { background-color: #ffffff}")
+
+    def alert(self):
+        msg = QMessageBox()
+        self.label.setText("")
+        msg.setWindowTitle("Attenzione!")
+        msg.setText("Il parco non è stato selezionato!")
+        msg.setIcon(QMessageBox.Icon.Critical)
+        msg.exec()
+
+
     def parcoChanger(self):
+        self.selected = True
         self.comboParchi.setStyleSheet("QComboBox { background-color: #bbf78f}")
-        if self.comboParchi.currentText()=="Parco Nazionale del Gran Paradiso":
+        if self.comboParchi.currentText()=="Parco Nazionale del Gran Paradiso" and self.selected==True:
             self.granParadiso()
-        elif self.comboParchi.currentText()=="Parco Nazionale del Cilento, Vallo di Diano e Alburni":
+        elif self.comboParchi.currentText()=="Parco Nazionale del Cilento, Vallo di Diano e Alburni" and self.selected==True:
             self.parcoCilento()
-        elif self.comboParchi.currentText()=="Parco Nazionale delle Cinque Terre":
+        elif self.comboParchi.currentText()=="Parco Nazionale delle Cinque Terre" and self.selected==True:
             self.parcoCinqueTerre()
+        else:
+            self.alert()
 
     def granParadiso(self):
+        self.pushFlora.clicked.disconnect(self.alert)
+        self.pushFauna.clicked.disconnect(self.alert)
+        self.pushActivity.clicked.disconnect(self.alert)
+        self.pushRegole.clicked.disconnect(self.alert)
         self.pushFlora.clicked.connect(lambda: self.label.setText(floraGranParadiso))
         self.pushFauna.clicked.connect(lambda: self.label.setText(faunaGranParadiso))
         self.pushActivity.clicked.connect(lambda: self.label.setText(activityGranParadiso))
         self.pushRegole.clicked.connect(lambda: self.label.setText(regoleGranParadiso))
     def parcoCilento(self):
+        self.pushFlora.clicked.disconnect(self.alert)
+        self.pushFauna.clicked.disconnect(self.alert)
+        self.pushActivity.clicked.disconnect(self.alert)
+        self.pushRegole.clicked.disconnect(self.alert)
         self.pushFlora.clicked.connect(lambda: self.label.setText(floraCilento))
         self.pushFauna.clicked.connect(lambda: self.label.setText(faunaCilento))
         self.pushActivity.clicked.connect(lambda: self.label.setText(activityCilento))
         self.pushRegole.clicked.connect(lambda: self.label.setText(regoleCilento))
     def parcoCinqueTerre(self):
+        self.pushFlora.clicked.disconnect(self.alert)
+        self.pushFauna.clicked.disconnect(self.alert)
+        self.pushActivity.clicked.disconnect(self.alert)
+        self.pushRegole.clicked.disconnect(self.alert)
         self.pushFlora.clicked.connect(lambda: self.label.setText(floraCinqueTerre))
         self.pushFauna.clicked.connect(lambda: self.label.setText(faunaCinqueTerre))
         self.pushActivity.clicked.connect(lambda: self.label.setText(activityCinqueTerre))
